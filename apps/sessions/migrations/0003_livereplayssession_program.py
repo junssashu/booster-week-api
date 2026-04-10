3f026beac0a28a40ac054457481003a9a58ab0ff
Juns Sashu <junssashu@gmail.com>
2026-04-10 13:39:36 +0100

    fix: correct model_name in session migration (livereplaysession not livereplayssession)

--- apps/sessions/migrations/0003_livereplayssession_program.py
+++ apps/sessions/migrations/0003_livereplayssession_program.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.AddField(
-            model_name='livereplayssession',
+            model_name='livereplaysession',
             name='program',
             field=models.ForeignKey(
                 blank=True,
