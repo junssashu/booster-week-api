e5b922f92f7bdcc161c2c09bbe213d747ec65569
Juns Sashu <junssashu@gmail.com>
2026-04-17 10:21:46 +0100

    feat: public mandataires list endpoint (no auth)

---
+++ apps/accounts/views_mandataires.py
@@ -0,0 +1,17 @@
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import User
+
+
+class PublicMandataireListView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        qs = User.objects.filter(is_mandataire=True).order_by('first_name', 'last_name')
+        data = [
+            {'id': u.id, 'firstName': u.first_name, 'lastName': u.last_name}
+            for u in qs
+        ]
+        return Response(data)
